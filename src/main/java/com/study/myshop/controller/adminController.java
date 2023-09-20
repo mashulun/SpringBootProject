e451b1e39c5c5ea57fbfd57e011d2f2557f5ad97
mashulun <872068891@qq.com>
2023-09-20 22:59:51 +0800

    主页,登录页面 主页,登录Controller

---
+++ src/main/java/com/study/myshop/controller/adminController.java
@@ -0,0 +1,37 @@
+package com.study.myshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 登录页面
+ * @author msl
+ * @version 1.0
+ * @create 2023-09-20 22:10
+ */
+@Controller
+@RequestMapping(value = "/admin")
+public class adminController  {
+
+
+    @RequestMapping(value ="/login")
+    public String index(Model model){
+        model.addAttribute("news","我的项目诞生了!");
+        return "admin/login";
+    }
+
+
+    /**
+     * 登灵提交方法对应表单控件name，
+     * 接收用户名和密码
+     *返回 null 就可以了，会被Security拦截掉的
+     * @param userName 用户名
+     * @param passWord 密码
+     * @return null
+     */
+    @RequestMapping(value = "/doLogin")
+    public String doLogin(String userName,String passWord){
+        return null;
+    }
+}
---
+++ src/main/java/com/study/myshop/controller/indexController.java
@@ -0,0 +1,23 @@
+package com.study.myshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 主页面
+ * @author msl
+ * @version 1.0
+ * @create 2023-09-20 21:30
+ */
+@Controller
+@RequestMapping(value = {"/","index"})
+public class indexController {
+
+    @RequestMapping(value = {"/","/index"})
+    public String index(){
+        return "index/index";
+    }
+
+
+
+}
